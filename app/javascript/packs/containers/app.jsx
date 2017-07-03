78de1e7a7986626be9ce01252ce133f99d6fc7ae
Earth <virudson@gmail.com>
2017-07-03 17:11:23 +0700

    Show Tweet data at app component

--- app/javascript/packs/containers/app.jsx
+++ app/javascript/packs/containers/app.jsx
@@ -1,11 +1,33 @@
-import React, { Component } from 'react'
+import React, {Component} from 'react'
 
 class App extends Component {
-    render() {
-        return(
-            <div>Hello {this.props.name}! Sugoii!!</div>
-        )
+  constructor(props) {
+    super(props)
+    this.state = { loading: true }
+  }
+
+  componentDidMount() {
+    fetch('/api/tweets/')
+      .then(response => response.json())
+      .then(response => {
+        console.log(response)
+        this.setState({tweets: response, loading: false})
+      })
+  }
+
+  render() {
+    const {loading, tweets} = this.state
+    if (loading) {
+      return <div>Loading...</div>
     }
+    return (
+    <ul>
+      {tweets.map(tweet => (
+        <li key={tweet.id}>{tweet.text}</li>
+      ))}
+    </ul>
+    )
+  }
 }
 
 export default App
