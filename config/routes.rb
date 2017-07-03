3afd19d3785c4bf690cffacfff4e25ca06792efd
Earth <virudson@gmail.com>
2017-07-03 16:14:31 +0700

    Init React component

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   namespace :api do
-    resources :tweets , only: :index
+    resources :tweets, only: :index
   end
+
+  resources :tweets, only: :index
+
+  root to: 'tweets#index'
 end
