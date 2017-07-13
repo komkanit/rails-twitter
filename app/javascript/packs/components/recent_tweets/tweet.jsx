19c7fa087a93110d6fb6a81df474fbc78ac5f9c9
Earth <virudson@gmail.com>
2017-07-13 15:04:49 +0700

    change following PR comment

--- app/javascript/packs/components/recent_tweets/tweet.jsx
+++ app/javascript/packs/components/recent_tweets/tweet.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Tweet = ({ tweet }) => {
   return (
     <div className="row justify-content-center mb-2">
-      <div className="col-6">
+      <div className="col-md-6">
         <div className="card">
           <div className="card-block">
             <h6 className="card-title">Tweet by Someone</h6>
--- app/javascript/packs/components/tweet_form.jsx
+++ app/javascript/packs/components/tweet_form.jsx
@@ -2,41 +2,25 @@ import React, { Component } from 'react'
 import PropTypes from 'prop-types'
 
 class TweetForm extends Component {
-  tweetMessageForm() {
-    return (
-      <div className="input-group input-group-lg">
-        <input
-          type="text"
-          value={this.props.tweetMessage}
-          className={`form-control ${this.props.formError ? 'form-control-danger' : ''}`}
-          placeholder="What's Happening?"
-          onChange={this.props.onInputChange}
-        />
-        {
-          this.props.formError &&
-          <div className="form-control-feedback">
-            Tweet message can't be blank.
-          </div>
-        }
-        <span className="input-group-btn">
-          <button
-            type="submit"
-            className="btn btn-primary"
-            onClick={this.props.onTweet}
-          >
-            Tweet
-                  </button>
-        </span>
-      </div>
-    )
-  }
-
   render() {
     return (
       <div className="row justify-content-center mb-3">
-        <div className="col-6">
+        <div className="col-md-6">
           <div className="card">
-            <div className="card-block">{this.tweetMessageForm()}</div>
+            <div className="card-block">
+              <div className={`input-group input-group-lg ${this.props.formError ? 'has-danger' : ''}`}>
+                <input
+                  type="text"
+                  value={this.props.tweetMessage}
+                  className={`form-control ${this.props.formError ? 'form-control-danger' : ''}`}
+                  placeholder="What's Happening?"
+                  onChange={this.props.onInputChange}
+                />
+                <span className="input-group-btn">
+                  <button type="submit" className="btn btn-primary" onClick={this.props.onTweet}>Tweet</button>
+                </span>
+              </div>
+            </div>
           </div>
         </div>
       </div>
